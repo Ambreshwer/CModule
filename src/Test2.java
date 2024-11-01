39c7099896e5d24898291f76508e1be43bf60d13
surya <surya@LAPTOP-36J5KNKJ>
2024-11-01 13:26:32 +0530

    Tester first commit......

---
+++ src/Test2.java
@@ -0,0 +1,13 @@
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		
+		div();
+	}
+	
+	public static void div() {
+		
+		System.out.println("Tester added his code first time");
+	}
+}
